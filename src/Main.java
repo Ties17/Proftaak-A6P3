743e2c4dc40eb483e11a5a5358b2375c5990038a
Ties <tiestienhoven@gmail.com>
2019-02-18 14:35:04 +0100

    basis voor json geadd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import javax.json.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
 
+    public static final String path = System.getProperty("user.dir");
 
+    public static final String Json_File = path + "\\Yowhatup.json";
+
+    public static void main(String[] args) {
+
+        try (
+                InputStream inputStream = new FileInputStream(Json_File);
+                JsonReader jsonReader = Json.createReader(inputStream);
+        ){
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 }

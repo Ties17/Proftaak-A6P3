1fec747057382034cd706a352414aa2a3bbfb14f
Ties <tiestienhoven@gmail.com>
2019-02-20 11:15:58 +0100

    switch case toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,26 @@ import java.io.InputStream;
 
 public class Main {
 
-    public static final String path = System.getProperty("user.dir");
-
-    public static final String Json_File = path + "\\Yowhatup.json";
+    public static final String Json_File = "C:\\Users\\tiest\\Documents\\School\\Periode 3\\Proftaak FP\\Code via Git\\ProftaakA6P3-JsonReader\\Proftaak-A6P3\\src\\jsonFiles\\Yowhatup.json";
 
     public static void main(String[] args) {
 
         try (
                 InputStream inputStream = new FileInputStream(Json_File);
                 JsonReader jsonReader = Json.createReader(inputStream);
-        ){
+        ) {
+            JsonObject jsonObject = jsonReader.readObject();
 
+            for ( String name: jsonObject.keySet()){
 
-
+                switch (jsonObject.get(name).getValueType()){
+                    case ARRAY:
+                    case NUMBER:
+                    case OBJECT:
+                    case STRING:
+                    case NULL:
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {

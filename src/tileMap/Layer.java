e157f8d389337274f9fb5c382220619c6ff959f8
Lavictus <mark2baaij@gmail.com>
2019-02-25 14:33:47 +0100

    Layer thereuhaveit

--- src/tileMap/Layer.java
+++ src/tileMap/Layer.java
@@ -1,11 +1,12 @@
 package tileMap;
 
 import javax.json.JsonArray;
+import javax.json.JsonObject;
 import java.util.Arrays;
 
 public class Layer {
 
-        private int[] data;
+        private JsonArray data;
         private int height;
         private int id;
         private String name;
@@ -16,23 +17,25 @@ public class Layer {
         private int x;
         private int y;
 
-        public Layer(JsonArray layer) {
-           
-            this.height = height;
-            this.id = id;
-            this.name = name;
-            this.opacity = opacity;
-            this.type = type;
-            this.visible = visible;
-            this.width = width;
-            this.x = x;
-            this.y = y;
+        public Layer(JsonObject layer) {
+
+            this.data = layer.getJsonArray("data");
+            this.height = layer.getInt("height");
+            this.id = layer.getInt("id");
+            this.name = layer.getString("name");
+            this.opacity = layer.getInt("opacity");
+            this.type = layer.getString("type");
+            this.visible = layer.getBoolean("visible");
+            this.width = layer.getInt("width");
+            this.x = layer.getInt("x");
+            this.y = layer.getInt("y");
+
         }
 
         @Override
         public String toString() {
             return "tileMap.Layer.Layer{" +
-                    "data=" + Arrays.toString(data) +
+                    "data=" + data.toString() +
                     "\n height=" + height +
                     "\n id=" + id +
                     "\n name='" + name + '\'' +
